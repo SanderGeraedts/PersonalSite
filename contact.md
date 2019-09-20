7089629d20b915445a4a8e2b8811341830dd8a2a
Sander Geraedts <sander.geraedts@sogeti.com>
2019-09-20 16:22:22 +0200

    Initial idea

---
+++ contact.md
@@ -0,0 +1,6 @@
+---
+title: Contact
+description: Hoe je het beste met Sander Geraedts in contact kan komen.
+layout: default
+---
+Contact
---
+++ js/main.js
@@ -0,0 +1,4 @@
+function toggleAvatarHover() {
+    const avatar = document.getElementById('avatar-spinner');
+    avatar.classList.toggle('hover');
+}
